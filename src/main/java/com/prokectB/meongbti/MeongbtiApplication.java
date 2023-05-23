0e3d01e088ff0cc076c45ca4ce94ea5272d8ca8f
Seoyoon <seoyoon047@gmail.com>
2023-05-23 16:48:29 +0900

    chore: application.yml 설정 추가,

---
+++ src/main/java/com/prokectB/meongbti/MeongbtiApplication.java
@@ -0,0 +1,19 @@
+package com.prokectB.meongbti;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class MeongbtiApplication  {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeongbtiApplication.class, args);
+	}
+
+
+}
